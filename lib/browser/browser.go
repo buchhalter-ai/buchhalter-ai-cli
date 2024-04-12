9bde79cb1e9eae303aef291af2a18c76166f689f
Martin Brüggemann <martin@brgmn.de>
2024-04-12 16:11:05 +0200

    TASK: Implement step.When.Url filter
    And skip second layer click if „value“ in downloadAll step is empty.

--- lib/browser/browser.go
+++ lib/browser/browser.go
@@ -133,6 +133,22 @@ func (b *BrowserDriver) RunRecipe(p *tea.Program, totalStepCount int, stepCountI
 		})
 
 		stepResultChan := make(chan utils.StepResult, 1)
+
+		// Check if step should be skipped
+		if step.When.URL != "" {
+			var currentURL string
+			if err := chromedp.Run(ctx, chromedp.Location(&currentURL)); err != nil {
+				// handle error
+			}
+
+			// Check if the current URL is not equal to step.When.URL
+			if currentURL != step.When.URL {
+				go func() {
+					stepResultChan <- utils.StepResult{Status: "success"}
+				}()
+			}
+		}
+
 		// Timeout recipe if something goes wrong
 		go func() {
 			switch action := step.Action; action {
@@ -352,10 +368,17 @@ func (b *BrowserDriver) stepDownloadAll(ctx context.Context, step parser.Step) u
 		b.logger.Debug("Executing recipe step ... trigger download click", "action", step.Action, "selector", n.FullXPath()+step.Value)
 		if err := chromedp.Run(ctx, fetch.Enable(), chromedp.Tasks{
 			chromedp.MouseClickNode(n),
-			chromedp.WaitVisible(n.FullXPath() + step.Value),
-			chromedp.Click(n.FullXPath() + step.Value),
 		}); err != nil {
 			return utils.StepResult{Status: "error", Message: err.Error()}
+		}
+
+		if step.Value != "" {
+			if err := chromedp.Run(ctx, fetch.Enable(), chromedp.Tasks{
+				chromedp.WaitVisible(n.FullXPath() + step.Value),
+				chromedp.Click(n.FullXPath() + step.Value),
+			}); err != nil {
+				return utils.StepResult{Status: "error", Message: err.Error()}
+			}
 		}
 		// Delay clicks to prevent too many downloads at once/rate limiting
 		time.Sleep(1500 * time.Millisecond)
